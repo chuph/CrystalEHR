587099ca0aa82eb8a5512b3e0f67580d1f37a3a7
Brandon <fulleb33@gmail.com>
2017-10-07 13:57:02 -0500

    tabsworking

--- app/components/children/Column2.js
+++ app/components/children/Column2.js
@@ -3,18 +3,21 @@ import React  from 'react';
 import PropTypes from 'prop-types';
 import Component from '../Main.js';
 
-class Columns extends React.Component {
-	render() {
-		return (<div className="panel">
 
+
+const Columns = props =>(
+	
+		<div className="panel">
 			<p><img src="Icons/Doctor_Tablet.jpeg"></img></p>
+			
+			<p><span onClick={() => props.col2Button1()}>{props.items[0]}</span></p>
+			<p><span onClick={() => props.col2Button2()}>{props.items[1]}</span></p>
+			<p><span onClick={() => props.col2Button3()}>{props.items[2]}</span></p>
+			
+		</div>
+	
+);
 
-			</div>);
-	}
-}
 
-Columns.PropTypes = {
-	text: PropTypes.string.isRequired,
-};
 
 module.exports = Columns;
